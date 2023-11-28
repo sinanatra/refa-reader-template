87264b4c7aa4c302f5887b81c234b6971d93b916
Giacomo Nanni <gn.nanni@gmail.com>
2023-11-28 11:18:44 +0100

    feat metadata

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { page } from '$app/stores';
+	import * as config from '@setup';
+
+    const essayData = [...$page.data.posts].find((d) => d.path.includes($page.params.slug));
+</script>
+
+<svelte:head>
+	<title>{essayData?.meta?.title}</title>
+	<meta name="description" content={essayData?.meta?.description || config.descriptionSeo} />
+	<meta property="og:url" content={$page.url.origin} />
+	<meta property="og:title" content={essayData?.meta?.title} />
+	<meta property="og:description" content={essayData?.meta?.description || config.descriptionSeo} />
+	<meta property="og:image" content={essayData?.meta?.cover|| config.imageSeo} />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta property="twitter:url" content={$page.url.origin} />
+	<meta name="twitter:title" content="Restaging Fashion" />
+	<meta name="twitter:description" content={essayData?.meta?.description || config.descriptionSeo} />
+	<meta name="twitter:image" content={essayData?.meta?.cover|| config.imageSeo} />
+</svelte:head>
+
+<slot />
