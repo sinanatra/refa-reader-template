ebbded3374c9ab8fe7da38fde1c2b87013b75b72
Giacomo Nanni <gn.nanni@gmail.com>
2023-11-28 15:42:01 +0100

    fix paths

--- src/components/Columns.svelte
+++ src/components/Columns.svelte
@@ -7,7 +7,7 @@
 <div class="columns">
 	{#each data.posts as post}
 		{#if post.meta.isPublic && post.meta.lang == lang}
-			<a href={base}/{post.path} target="_self">
+			<a href="{base}{post.path}" target="_self">
 				<div class="column" style="--meta-color: {post.meta.color}">
 					<h2>
 						{post.meta.title}
