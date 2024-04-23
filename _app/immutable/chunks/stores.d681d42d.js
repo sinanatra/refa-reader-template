e2aec8959ebe403b73f006d496561db4c0ff68f6
Giacomo Nanni <gn.nanni@gmail.com>
2024-04-23 18:49:40 +0200

    Updates

---
+++ _app/immutable/chunks/stores.d681d42d.js
@@ -0,0 +1,1 @@
+import"./index.19860723.js";import{d as e}from"./singletons.360c7908.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
